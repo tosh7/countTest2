077f823669d989b128929eeb650f473c4ea4f37b
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-11-19 14:35:28 +0900

    Initial Commit

---
+++ countTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countTest
+//
+//  Created by Satoshi Komatsu on 2017/11/19.
+//  Copyright © 2017年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
